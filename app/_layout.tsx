4ec57054303d4801c1405d7872014220c3e42cd3
Esteban Vega <iestebanvega1@gmail.com>
2024-09-11 11:22:13 -0500

    add expo-router

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Entypo, Feather } from "@expo/vector-icons";
+import { Stack, Tabs } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
